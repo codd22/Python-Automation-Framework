4bf0a20ff29b7df0f6291c5257f0bce773375ed8
codd22 <chauhanabhi2210@gmail.com>
2025-05-01 08:02:01 +0530

    updated step logic to common logic

--- features/steps/iotDashboardStep.py
+++ features/steps/iotDashboardStep.py
@@ -3,5 +3,5 @@ from behave import *
 
 @given('I verify {element} is {status}')
 def i_verify_element_status(context, element, status):
-    if element == 'lightCardStatus':
-        assert context.iotDashboardPage.getText(context.iotDashboardPage.lightCardStatus) == status
+    locator = getattr(context.iotDashboardPage, element, None)
+    assert context.iotDashboardPage.getText(locator) == status
