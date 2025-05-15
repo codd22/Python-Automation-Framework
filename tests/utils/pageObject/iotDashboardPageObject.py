3dd1a6cd72bb449b00ce524cbd9ca6b01a55d442
codd22 <chauhanabhi2210@gmail.com>
2025-05-15 22:34:36 +0530

    added all cards element

--- tests/utils/pageObject/iotDashboardPageObject.py
+++ tests/utils/pageObject/iotDashboardPageObject.py
@@ -5,7 +5,6 @@ class IotDashboardPageObject:
     def __init__(self, page: Page):
         self.page = page
         self.lightCard = self.page.locator('[ng-reflect-title="Light"]')
-        self.lightCardStatus = self.lightCard.locator('.status')
         self.rollerShadesCard = self.page.locator('[ng-reflect-title="Roller Shades"]')
-        self.rollerShadesCardStatus = self.rollerShadesCard.locator('.status')
-        
+        self.wirelessAudioCard = self.page.locator('[ng-reflect-title="Wireless Audio"]')
+        self.coffeeMakerCard = self.page.locator('[ng-reflect-title="Coffee Maker"]')
