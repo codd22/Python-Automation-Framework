395f63c9fd5bd6f6ac9d2756f1f635eebdadfda0
codd22 <chauhanabhi2210@gmail.com>
2025-05-16 23:08:15 +0530

    updated currentPage references

--- features/steps/iotDashboardStep.py
+++ features/steps/iotDashboardStep.py
@@ -4,5 +4,5 @@ from playwright.sync_api import Locator
 
 @given('I verify {element} status is {status}')
 def i_verify_element_status(context, element, status):
-    loc: Locator = getattr(context.iotDashboardPage, element, None)
-    assert context.iotDashboardPage.getText(loc.locator('.status')) == status
+    loc: Locator = getattr(context.currentPage, element, None)
+    assert context.currentPage.getText(loc.locator('.status')) == status
