f2912247535f54b13229576a9b55884c78298bf6
codd22 <chauhanabhi2210@gmail.com>
2025-05-17 16:05:41 +0530

    updated other steps functionality and added click generic step

--- features/steps/commonStep.py
+++ features/steps/commonStep.py
@@ -1,12 +1,14 @@
 from behave import *
+from playwright.sync_api import Locator
 
 
 @given('I open the url "{url}"')
 def i_open_the_url(context, url: str):
     context.basePage.navigate(url)
+    i_verify_the_page(context, 'IotDashboardPage')
 
 
-@given('I verify the {page} loaded')
+@step('I verify the {page} loaded')
 def i_verify_the_page(context, page):
     # Convert first letter to lowercase
     page_variable = page[0].lower() + page[1:]
@@ -18,3 +20,9 @@ def i_verify_the_page(context, page):
     sub_url = getattr(context.currentPage, 'url')
     # Verify the sub url contains in current url
     assert sub_url in current_url, f"Expected '{sub_url}' to be in URL '{current_url}'"
+
+
+@when('I click on {element}')
+def i_click_on_element(context, element):
+    loc: Locator = getattr(context.currentPage, element, None)
+    context.currentPage.clickOnElement(loc)
