56f2d1f5cc4bffd91d9049450b111ee857f4a388
codd22 <chauhanabhi2210@gmail.com>
2025-05-17 16:06:05 +0530

    updated step type

--- features/steps/iotDashboardStep.py
+++ features/steps/iotDashboardStep.py
@@ -2,7 +2,7 @@ from behave import *
 from playwright.sync_api import Locator
 
 
-@given('I verify {element} status is {status}')
+@step('I verify {element} status is {status}')
 def i_verify_element_status(context, element, status):
     loc: Locator = getattr(context.currentPage, element, None)
     assert context.currentPage.getText(loc.locator('.status')) == status
